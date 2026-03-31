1e50b2b1aca6032ad529c50987b6e6f60589b403
Hasse Wilson <hasse@ramlev.dk>
2026-03-31 16:10:48 +0200

    Add git_current_branch and __git_prompt_git helper functions

--- zsh/.zsh/functions.zsh
+++ zsh/.zsh/functions.zsh
@@ -6,7 +6,19 @@ function digga() {
 function removehost() {
    ssh-keygen -R "$1"
 }
-
+function __git_prompt_git() {
+  GIT_OPTIONAL_LOCKS=0 command git "$@"
+}
+function git_current_branch() {
+  local ref
+  ref=$(__git_prompt_git symbolic-ref --quiet HEAD 2> /dev/null)
+  local ret=$?
+  if [[ $ret != 0 ]]; then
+    [[ $ret == 128 ]] && return  # no git repo.
+    ref=$(__git_prompt_git rev-parse --short HEAD 2> /dev/null) || return
+  fi
+  echo ${ref#refs/heads/}
+}
 _fzf_compgen_dir() {
   fd --type d --hidden --follow --exclude .git . "$1" 2>/dev/null
 }
